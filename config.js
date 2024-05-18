c90c16ee11d6e7c66064d06b3044ceaaa8505927
Xenon <49691279+XenonLUA@users.noreply.github.com>
2024-05-18 15:56:56 +0700

    se

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const main_url = 'https://Key.XenonHUB.io'; // The Main URl/Root Url example: ht
 const linkvertise_ID = '1092296'; // Your linkvertise ID, you need it for to get paid from linkvertise.
 const adminWhitelist = ['439705027083501579']; // Your Discord ID, and other admins who has perms to accses the Admin Panel.
 const DISCORD_CLIENT_ID = '1086599423305461790'; // Your Discord Client ID for Discord Admin Login.
-const DISCORD_CLIENT_SECRET = 'wQiHdIG-dFkQBOk_qxQOPvYW4KEV2WfD'; // Your Discord Client Secret for Discord Admin Login.
+const DISCORD_CLIENT_SECRET = 'rjabQU7bv-4YDs0UrBFEqWEroshDtpfM'; // Your Discord Client Secret for Discord Admin Login.
 const secret = 'QFBMgpRoA3mdcyjOyw0isddssLh/A/uYY+x1dyq8drg8+2Zs='; // For More Security, secret for admin loging with discord.
 const DISCORD_WEBHOOK_LOGIN = 'https://discord.com/api/webhooks/1214531415568293959/PfBG06D8Z4U8PDPuDvWWwHAhcNfokKt3Z-DL025IZrrCcIzSPlyHbmejD9H_JSLgtpyz'; // For admin logs.
 const DISCORD_WEBHOOK_KEYSYSTEM_LOGS = 'https://discord.com/api/webhooks/1214531415568293959/PfBG06D8Z4U8PDPuDvWWwHAhcNfokKt3Z-DL025IZrrCcIzSPlyHbmejD9H_JSLgtpyz'; // This is the when a user goes to the next checkpoint, and when the user gets the key.
