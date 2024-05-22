e0819d6201f072a17e36e9fe030823b98eeb6147
Xenon <49691279+XenonLUA@users.noreply.github.com>
2024-05-22 13:01:53 +0700

    ddasdasd

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ const adminWhitelist = ['439705027083501579']; // Your Discord ID, and other adm
 const DISCORD_CLIENT_ID = '1086599423305461790'; // Your Discord Client ID for Discord Admin Login.
 const DISCORD_CLIENT_SECRET = 'U6s0WsVIFdul_b5zPy8ec21RYTmjvwkU'; // Your Discord Client Secret for Discord Admin Login.
 const secret = 'QFBMgpRoA3mdcyjOyw0isddssLh/A/uYY+x1dyq8drg8+2Zs='; // For More Security, secret for admin loging with discord.
-const DISCORD_WEBHOOK_LOGIN = 'https://discord.com/api/webhooks/1214531415568293959/PfBG06D8Z4U8PDPuDvWWwHAhcNfokKt3Z-DL025IZrrCcIzSPlyHbmejD9H_JSLgtpyz'; // For admin logs.
-const DISCORD_WEBHOOK_KEYSYSTEM_LOGS = 'https://discord.com/api/webhooks/1214531415568293959/PfBG06D8Z4U8PDPuDvWWwHAhcNfokKt3Z-DL025IZrrCcIzSPlyHbmejD9H_JSLgtpyz'; // This is the when a user goes to the next checkpoint, and when the user gets the key.
+const DISCORD_WEBHOOK_LOGIN = 'https://discord.com/api/webhooks/1242717722891259904/B0xPBmiQE5ot9oxGMKgktu3VqTYHW9QeaO5vd1-QeANg-zZcoBF1i7_2-T0ofLa6bfeK'; // For admin logs.
+const DISCORD_WEBHOOK_KEYSYSTEM_LOGS = 'https://discord.com/api/webhooks/1242717722891259904/B0xPBmiQE5ot9oxGMKgktu3VqTYHW9QeaO5vd1-QeANg-zZcoBF1i7_2-T0ofLa6bfeK'; // This is the when a user goes to the next checkpoint, and when the user gets the key.
 
 export { main_url, linkvertise_ID, adminWhitelist, DISCORD_CLIENT_ID, DISCORD_CLIENT_SECRET, secret, DISCORD_WEBHOOK_LOGIN, DISCORD_WEBHOOK_KEYSYSTEM_LOGS };
