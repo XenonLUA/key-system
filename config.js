17b8898c3773e4d7bdbf8b6cc95c67c24f95b5fa
Xenon <49691279+XenonLUA@users.noreply.github.com>
2024-10-31 18:43:32 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,2 @@
-const main_url = 'https://key-system-mauve.vercel.app/'; // The Main URl/Root Url example: https://example.com
-const linkvertise_ID = '1092296'; // Your linkvertise ID, you need it for to get paid from linkvertise.
-const adminWhitelist = ['439705027083501579']; // Your Discord ID, and other admins who has perms to accses the Admin Panel.
-const DISCORD_CLIENT_ID = '1086599423305461790'; // Your Discord Client ID for Discord Admin Login.
-const DISCORD_CLIENT_SECRET = 'U6s0WsVIFdul_b5zPy8ec21RYTmjvwkU'; // Your Discord Client Secret for Discord Admin Login.
-const secret = 'QFBMgpRoA3mdcyjOyw0isddssLh/A/uYY+x1dyq8drg8+2Zs='; // For More Security, secret for admin loging with discord.
-const DISCORD_WEBHOOK_LOGIN = 'https://discord.com/api/webhooks/1242717722891259904/B0xPBmiQE5ot9oxGMKgktu3VqTYHW9QeaO5vd1-QeANg-zZcoBF1i7_2-T0ofLa6bfeK'; // For admin logs.
-const DISCORD_WEBHOOK_KEYSYSTEM_LOGS = 'https://discord.com/api/webhooks/1242717722891259904/B0xPBmiQE5ot9oxGMKgktu3VqTYHW9QeaO5vd1-QeANg-zZcoBF1i7_2-T0ofLa6bfeK'; // This is the when a user goes to the next checkpoint, and when the user gets the key.
 
 export { main_url, linkvertise_ID, adminWhitelist, DISCORD_CLIENT_ID, DISCORD_CLIENT_SECRET, secret, DISCORD_WEBHOOK_LOGIN, DISCORD_WEBHOOK_KEYSYSTEM_LOGS };
